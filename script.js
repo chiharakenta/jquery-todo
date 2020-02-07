af8f9f2d0bbfce2024b5a21241ce6827d6904bf7
chiharakenta <chiharakenta0519@gmail.com>
2020-02-07 16:39:37 +0900

    update script.js

--- script.js
+++ script.js
@@ -1,38 +1,46 @@
 $(function() {
   // 有料登録ポップアップの表示
-  setTimeout(() => {
+  setTimeout(function() {
     $('#modal').fadeIn();
   }, 5000);
-  $('.closeModalButton').click(() => {
+  $('.closeModalButton').click(function() {
     $('#modal').fadeOut();
   });
 
   // ToDoの追加
-  $('#addTodoButton').click((e) => {
-    e.preventDefault();
-    let todoTitle = $('#todoTitleInput').val();
-    let todoContent = $('#todoContentTextarea').val();
-    $('#todoTitleInput').val('');
-    $('#todoContentTextarea').val('');
+  $('#addTodoButton').click(function() {
+    const todoTitle = $('#todoTitleInput').val();
+    const todoContent = $('#todoContentTextarea').val();
+    clearForm();
 
-    let todoList = $('#todoList');
-    let newTodo = `<li class="text-muted pt-3 d-flex align-items-center justify-content-between"><p class="pb-3 mb-0 small lh-125 border-bottom border-gray"><strong class="d-block text-gray-dark pb-1">${todoTitle}</strong>${todoContent}</p><div class="complete-button btn btn-primary btn-raised">完了</div></li>`;
+    const todoList = $('#todoList');
+    const newTodo = `<li class="text-muted pt-3 d-flex align-items-center justify-content-between">`
+                  + `<p class="pb-3 mb-0 small lh-125 border-bottom border-gray">`
+                  + `<strong class="d-block text-gray-dark pb-1">${todoTitle}</strong>`
+                  + `${todoContent}</p>`
+                  + `<div class="complete-button btn btn-primary btn-raised">完了</div>`
+                  + `</li>`;
     todoList.prepend(newTodo);
+    return false;
   });
 
   // ToDo追加フォームの内容のクリア
-  $('#clearFormButton').click((e) => {
-    e.preventDefault();
-    $('#todoTitleInput').val('');
-    $('#todoContentTextarea').val('');
+  $('#clearFormButton').click(function() {
+    clearForm();
+    return false;
   });
 
   // ToDoの完了
-  $(document).on('click', '.complete-button', (e) => {
+  $(document).on('click', '.complete-button', function() {
     if(confirm('このタスクを完了してよろしいですか？')){
-      $(e.currentTarget).parent().remove();
+      $(this).parent().remove();
     } else {
       return false;
     }
   });
+
+  function clearForm() {
+    $('#todoTitleInput').val('');
+    $('#todoContentTextarea').val('');
+  }
 });
